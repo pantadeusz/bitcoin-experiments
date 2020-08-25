7190f9d084ea37892fd3928e8817d86ef465122b
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2020-08-25 21:56:42 +0200

    cleaner arguments - no whitespaces and enter characters, except for the message - fix

--- op_return_cpp_nodejs/generateopreturn.js
+++ op_return_cpp_nodejs/generateopreturn.js
@@ -10,7 +10,7 @@ function generatetransaction(idx, backaddr, amountback, utxo, message, ourwif) {
     //let backaddr='tb1qs8lhy6fqznclyuuv8ct2zsn8rr9j9ukdn5m72p';
     //let amountback=0.0009;
     //let utxo='26a1a95089d1d84b7f17faef40c3c62eb0d677d8ff72367535062d0f4d31a229';
-    let generate_cmnd = `./my_addr -idx ${idx} -backaddr ${backaddr.replace(/\s/g,'')} -backsats ${amountback.replace(/\s/g,'')} -utxo ${utxo.replace(/\s/g,'')} -msg '${message}' -wif ${ourwif.replace(/\s/g,'')} -onlytx`
+    let generate_cmnd = `./my_addr -idx ${idx} -backaddr ${backaddr} -backsats ${amountback} -utxo ${utxo} -msg ${message} -wif ${ourwif} -onlytx`
     console.log(generate_cmnd);
     exec(generate_cmnd, (error, signedrawtransaction, stderr) => {
         console.log(`signedrawtransaction: "${signedrawtransaction}"`);
@@ -38,7 +38,7 @@ exec('bitcoin-cli listunspent', (error, unspentlist, stderr) => {
             console.log(process.argv[2]);
             // now let's get our WIF for the address to spend
             exec(`bitcoin-cli dumpprivkey ${tx.address}`, (error, ourwif, stderr) => {
-                generatetransaction(tx.vout, tx.address, tx.amount - 0.00000500, tx.txid, process.argv[2], ourwif);
+                generatetransaction(tx.vout, tx.address, tx.amount - 0.00000500, tx.txid, process.argv[2], ourwif.replace(/\s/g,''));
             });
         } else {
             console.log("give me message");
