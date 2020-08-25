776041ce3b9adf06a5384e858217832b5550ebe2
pantadeusz <pantadeusz+git@pjwstk.edu.pl>
2020-08-25 21:54:40 +0200

    cleaner arguments - no whitespaces and enter characters, except for the message

--- op_return_cpp_nodejs/generateopreturn.js
+++ op_return_cpp_nodejs/generateopreturn.js
@@ -10,7 +10,7 @@ function generatetransaction(idx, backaddr, amountback, utxo, message, ourwif) {
     //let backaddr='tb1qs8lhy6fqznclyuuv8ct2zsn8rr9j9ukdn5m72p';
     //let amountback=0.0009;
     //let utxo='26a1a95089d1d84b7f17faef40c3c62eb0d677d8ff72367535062d0f4d31a229';
-    let generate_cmnd = `./my_addr -idx ${idx} -backaddr ${backaddr} -backsats ${amountback} -utxo ${utxo} -msg ${message} -wif ${ourwif} -onlytx`
+    let generate_cmnd = `./my_addr -idx ${idx.replace(/\s/g,'')} -backaddr ${backaddr.replace(/\s/g,'')} -backsats ${amountback.replace(/\s/g,'')} -utxo ${utxo.replace(/\s/g,'')} -msg '${message}' -wif ${ourwif.replace(/\s/g,'')} -onlytx`
     console.log(generate_cmnd);
     exec(generate_cmnd, (error, signedrawtransaction, stderr) => {
         console.log(`signedrawtransaction: "${signedrawtransaction}"`);
@@ -19,7 +19,6 @@ function generatetransaction(idx, backaddr, amountback, utxo, message, ourwif) {
                 console.log("TXID", result);
                 console.log("error", error);
                 console.log("stderr", stderr);
-
             });
         } else {
             console.log("error signing transaction.", signedrawtransaction);
