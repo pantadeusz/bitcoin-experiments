ed33b21f79d12d42fabcea5a68182960c2d0daa2
t4deusz <pantadeusz+git@pjwstk.edu.pl>
2020-08-25 11:00:29 +0200

    towards raw tx in c++

--- op_return_cpp_nodejs/generateopreturn.js
+++ op_return_cpp_nodejs/generateopreturn.js
@@ -10,7 +10,7 @@ function generatetransaction(idx, backaddr, amountback, utxo, message) {
     //let backaddr='tb1qs8lhy6fqznclyuuv8ct2zsn8rr9j9ukdn5m72p';
     //let amountback=0.0009;
     //let utxo='26a1a95089d1d84b7f17faef40c3c62eb0d677d8ff72367535062d0f4d31a229';
-    let generate_cmnd = `./my_addr -idx ${idx} -backaddr ${backaddr} -backsats ${amountback} -utxo ${utxo} -msg ${message}| grep sendrawtransaction`
+    let generate_cmnd = `./my_addr -idx ${idx} -backaddr ${backaddr} -backsats ${amountback} -utxo ${utxo} -msg ${message}`
     console.log(generate_cmnd);
     exec(generate_cmnd, (error, rawtxunsigned, stderr) => {
         console.log("DONE:", rawtxunsigned);
